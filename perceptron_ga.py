5bc24cbdd608334791e1b63c81c926e40315ff0c
Annanda Sousa <annanda.sousa@gmail.com>
2016-05-10 21:12:42 -0300

    finalizando a primeira versão

--- perceptron_ga.py
+++ perceptron_ga.py
@@ -12,38 +12,50 @@ class PerceptronGA:
     number_of_features = None
 
     def __init__(self,
+                 csv_train_path,
                  population_size=5,
                  number_of_generations=10,
                  mutation_chance=0.1,
-                 crossover_chance=0.8
+                 crossover_chance=0.8,
                  ):
-
+        self.csv_train_path = csv_train_path
         self.population_size = population_size
         self.number_of_generations = number_of_generations
         self.crossover_chance = crossover_chance
         self.mutation_chance = mutation_chance
 
-    def sort_by_best(self, population, x, y):
-        pop_with_fit = [(ind, self.fitness_function(ind, x, y)) for ind in population]
-        sorted_population = sorted(pop_with_fit, key=lambda ind_fit: ind_fit[1])
+        self.read_csv()
+        self.fit()
 
-        z = zip(*sorted_population)
-        return next(z), next(z) # list de individuos e lista de suas fits
-
-    def fit(self, x, y):
+    def fit(self):
         population = self.create_initial_population()
 
         for generation in range(self.number_of_generations):
-            population = self.select(population, x, y)
-            #self.crossover(population)
-            #self.mutate(population)
+            population = self.select(population)
+            population = self.crossover(population)
+            population = self.mutate(population)
 
-            #best_individual = self.sort_by_best(population)[0]
-            #self.w0 = best_individual[0]
-            #self.w = best_individual[1:]
+            sorted_population = self.sort_by_best(population)
+            best_individual = sorted_population[0]
+            self.w0 = best_individual[0]
+            self.w = best_individual[1:]
 
-    def select(self, population, x, y):
-        sorted_population, fits = self.sort_by_best(population, x, y)
+    def sort_by_best(self, population):
+        population_with_fit = []
+
+        # cria uma lista de tuplas (individuo, valor_fit) para toda a populacao
+        for individual in population:
+            population_with_fit.append((individual, self.fitness_function(individual, self.x, self.y)))
+
+        sorted_population = sorted(population_with_fit, key=lambda individual_fit: individual_fit[1])
+
+        # separa as tuplas anteriores em duas duplas
+        z = zip(*sorted_population)
+        # list de individuos e lista de suas fits
+        return next(z), next(z)
+
+    def select(self, population):
+        sorted_population, fits = self.sort_by_best(population)
 
         total = 0
         for i in range(len(sorted_population)):
@@ -51,7 +63,7 @@ class PerceptronGA:
             fits[i] = total
 
         selected = []
-        for i in range(len(sorted_population)-1):
+        for i in range(len(sorted_population)):
             n = random.uniform(1, total)
             for j in range(len(fits)):
                 if n <= fits[j]:
@@ -60,13 +72,13 @@ class PerceptronGA:
 
         return selected
 
-    def crossover(self, population, chance = 0.8):
+    def crossover(self, population):
         for i in range(0, len(population), 2):
             if i+1 == len(population): #população impar
                 # o ultimo continua
                 continue
             random.seed()
-            if random.random() > chance: # 80% de chance de crossover
+            if random.random() > self.crossover_chance:
                 continue
 
             ind1 = population[i]
@@ -76,6 +88,8 @@ class PerceptronGA:
 
             population[i] = ind1[0:point] + ind2[point:]
             population[i+1] = ind2[0:point] + ind1[point:]
+
+        return population
 
     def mutate(self, population):
         for i in range(len(population)):
@@ -88,16 +102,17 @@ class PerceptronGA:
             individual[mutation_position] = random.choice(range(auxiliar_list[mutation_position]+1))
 
             population[i] = individual
+        return population
 
-    def fitness_function(self, individual, x, y):
+    def fitness_function(self, individual):
         erro_total = 0
-        for i, example in enumerate(x):
+        for i, example in enumerate(self.x):
             y_estimado = self.evaluate(individual, example)
-            erro = self.calcula_erro(y_estimado, y[i])
+            erro = abs(self.calcula_erro(y_estimado, self.y[i]))
             erro_total += erro
 
-        max_error = len(x) # o erro maximo por exemplo é 1
-        return max_error-erro_total
+        max_error = len(self.x) # o erro maximo por exemplo é 1
+        return max_error - erro_total
 
     def create_initial_population(self):
         population = []
@@ -136,3 +151,7 @@ class PerceptronGA:
                 self.y.append(row[3])
                 self.x.append(xi)
         self.number_of_features = len(self.x[0])
+
+p = PerceptronGA('/home/annanda/Documentos/mineracao/dataset.csv')
+print(p.w)
+print(p.w0)
